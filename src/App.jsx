670c374c3047d54505c771f68dfca5eae4057fb2
Kortsa <leoklomegah8@gmail.com>
2024-11-03 11:56:01 +0000

    added the login form and created folder for admin

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Welcome from "./Pages/Welcome/Welcome";
 import Events from "./Pages/Visitors/Events";
 import YoungAndLoudForm from "./EventForms/YoundAndLoud/YoungAndLoudForm";
+import AdminHome from "./Components/Admin/AdminHome/AdminHome";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/events" element={ <Events /> } />
         <Route path="/young_loud_form" element={ <YoungAndLoudForm /> } />
+        <Route path="/admin/home" element={ <AdminHome /> } />
       </Routes>
      
     </>
--- src/Pages/Welcome/Welcome.jsx
+++ src/Pages/Welcome/Welcome.jsx
@@ -1,12 +1,71 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Modal from "react-modal";
 import logo from "../../assets/N8.png";
+import { AiOutlineEye } from "react-icons/ai";
+import { AiFillEyeInvisible } from "react-icons/ai";
+import Swal from "sweetalert2";
+
 import "../Welcome/Welcome.css";
-import { useNavigate, Link } from "react-router-dom";
+
+// Set the app element for accessibility
+Modal.setAppElement("#root");
 
 const Welcome = () => {
   const navigate = useNavigate();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const eventBtn = () => {
     navigate("/events");
   };
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleLoginClick = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setTimeout(() => {
+      if (email === "admin@nodeeight" && password === "randomBytes") {
+        setLoading(false);
+        Swal.fire({
+          title: "Tym Sync",
+          text: `You've Successfully Logged in as ${"Admin"}`,
+          icon: "success",
+          customClass: {
+            title: "font-quicksand",
+            text: "font-quicksand",
+          },
+        });
+        navigate("/admin/home");
+      } else {
+        Swal.fire({
+          title: "Tym Sync",
+          text: `No user found, try again`,
+          icon: "error",
+          customClass: {
+            title: "font-quicksand",
+            text: "font-quicksand",
+          },
+        });
+        setLoading(false);
+        return;
+      }
+
+      // After loading, navigate to the next page
+      setLoading(false);
+      // Navigate logic here
+    }, 2000); // Simulated loading time: 2 seconds
+  };
+
   return (
     <>
       <div className="contents">
@@ -17,7 +76,7 @@ const Welcome = () => {
         </div>
         <div className="welcomeCard">
           <h2>Welcome</h2>
-          <div className="card">
+          <div className="card" onClick={openModal}>
             <h1>Login</h1>
             <p>Admin, staff and students</p>
           </div>
@@ -29,6 +88,44 @@ const Welcome = () => {
           </div>
         </div>
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Login Modal"
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <h2>Login</h2>
+        <form className="login-form" onSubmit={(e) => handleLoginClick(e)}>
+          <div className="form-group">
+            {/* <MdPerson className="icon" /> */}
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter Student/Staff/Admin ID"
+              required
+            />
+          </div>
+          <div className="form-group">
+            {/* <AiOutlineEye className="icon" /> */}
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              required
+            />
+          </div>
+
+          <a>Forget Password</a>
+          <button type="submit" className="btn" disabled={loading}>
+            {loading ? "Please wait.. logging in" : "Login"}
+          </button>
+        </form>
+      </Modal>
     </>
   );
 };
